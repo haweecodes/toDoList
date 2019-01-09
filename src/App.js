5295b1c22a4f98cbc5900141239096ef35ab0657
Sajib Hawee <shawee@iquantile.com>
2019-01-09 16:36:30 +0600

    init

--- src/App.js
+++ src/App.js
@@ -1,28 +1,84 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import {connect} from 'react-redux';
+import {addTodo} from './action'
+import './App.css'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props)
+        this.state = {
+            todolist: [],
+            value: ''
+        }
+        this.handleAdd = this.handleAdd.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleAdd(e) {
+        e.preventDefault();
+        this.props.dispatch(addTodo(this.state.value))
+        if (this.state.value.trim() !== "") {
+            this.setState(state => ({
+                todolist: state.todolist.concat(this.state.value),
+                value: ''
+            }))
+        }
+    }
+
+    handleChange(e) {
+        this.setState({
+            value: e.target.value
+        })
+    }
+
+    handleDelete = (value) => {
+        const filteredItems = this.state.todolist.filter(item => {
+            return item !== value
+        })
+        this.setState({
+            todolist: filteredItems,
+        })
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <main className={'we-container'}>
+
+                        <input className={'we-textBox'} type="text" value={this.state.value}
+                               onChange={this.handleChange}/>
+                        <button className={'we-button'} onClick={this.handleAdd}>
+                            <div className={'we-button-content'}>
+                                Add
+                            </div>
+                        </button>
+
+                        <div className={'we-list-container'}>
+                            <List items={this.state.todolist} deleteItem={this.handleDelete}/>
+                        </div>
+
+
+                </main>
+            </div>
+        );
+    }
 }
 
-export default App;
+class List extends Component {
+    render() {
+        return (
+            <ul>
+                {
+                    this.props.items.map((item, index) => (
+                        <li key={index}>{index + 1 + '.'} {item} <span
+                            onClick={() => this.props.deleteItem(item)}>x</span></li>
+                    ))
+                }
+            </ul>
+        )
+    }
+}
+
+export default connect()(App);
